1d59e393ca8d34a8a11a84ae53d0b2ccf57cc3f0
Rick Slotboom <rick.slotboom@hotmail.com>
2025-11-15 12:16:28 +0100

    menukaart toegevoegd

--- src/data/menuData.ts
+++ src/data/menuData.ts
@@ -1,7 +1,29 @@
 import { Dish } from "../types";
 
 export const MENU: Dish[] = [
-  { id: "d01", name: "Kip met groenten", image: "/images/kip.jpg", price: 9.5 },
-  { id: "d02", name: "Pasta bolognese", image: "/images/pasta.jpg", price: 8.0 },
-  { id: "d03", name: "Frietjes", image: "/images/friet.jpg", price: 3.5 },
+  { id: "d01", name: "Ontbijtplankje Speciaal", image: "/images/ontbijtplankje.jpg", price: 15.95 },
+  { id: "d02", name: "Lekker eitje als ontbijtje", image: "/images/lekker-eitje.jpg", price: 10.95 },
+  { id: "d03", name: "Pannenkoeken", image: "/images/pannenkoeken.jpg", price: 8.95 },
+  { id: "d04", name: "Yoghurt met fruit & granola", image: "/images/yoghurt-granola.jpg", price: 8.95 },
+  { id: "d05", name: "Verse jus d’orange", image: "/images/jus.jpg", price: 5.0 },
+  { id: "d06", name: "Smoothie van de dag", image: "/images/smoothie.jpg", price: 6.5 },
+
+  { id: "d07", name: "Tosti Speciaalzaak", image: "/images/tosti.jpg", price: 8.5 },
+  { id: "d08", name: "Panini Caprése", image: "/images/panini-caprese.jpg", price: 8.5 },
+  { id: "d09", name: "Panini Brie", image: "/images/panini-brie.jpg", price: 8.5 },
+
+  { id: "d10", name: "Tomatensoep", image: "/images/tomatensoep.jpg", price: 7.95 },
+  { id: "d11", name: "Soep van de dag", image: "/images/soep-van-de-dag.jpg", price: 7.95 },
+
+  { id: "d12", name: "Poké Bowl van Merel", image: "/images/pokebowl.jpg", price: 14.5 },
+  { id: "d13", name: "Tonijnsalade Speciaalzaak", image: "/images/tonijnsalade.jpg", price: 14.5 },
+
+  { id: "d14", name: "Bordje Ongezond", image: "/images/bordje-ongezond.jpg", price: 11.95 },
+  { id: "d15", name: "Gebakken Fricandeau met champignons", image: "/images/fricandeau.jpg", price: 13.5 },
+  { id: "d16", name: "Een ei hoort erbij", image: "/images/ei-hoort-erbij.jpg", price: 10.95 },
+  { id: "d17", name: "Kip cashew speciaal", image: "/images/kip-cashew.jpg", price: 13.5 },
+
+  { id: "d18", name: "Pittige tonijn", image: "/images/pittige-tonijn.jpg", price: 10.95 },
+  { id: "d19", name: "Het gezonde broodje", image: "/images/gezonde-broodje.jpg", price: 10.95 },
+  { id: "d20", name: "Heerlijke Bagel", image: "/images/bagel.jpg", price: 9.95 }
 ];
